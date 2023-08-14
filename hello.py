995d5a1066f6f612fce8afc064461f51b07a92d9
johnlennyt5 <johnlennyt@gmail.com>
2023-08-14 16:35:45 -0400

    added python hello world

---
+++ hello.py
@@ -0,0 +1,19 @@
+# ask user for their name and print it out
+# name = input("what's your name? ").strip().title()
+
+#split user's name into first and last name
+# first, last = name.split()
+
+
+
+# say hello to the user
+# print (f"hello, {name}")
+ 
+def main():
+  name = input("what's your name? ")
+  hello(name)
+
+def hello(to="wolrd"):
+  print("hello, to")
+
+main()
