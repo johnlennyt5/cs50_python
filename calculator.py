32efe29667bf09a5504cdc22df9ccf500cff5bdf
johnlennyt5 <johnlennyt@gmail.com>
2023-08-15 16:41:44 -0400

    added calacutor

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# x = float(input("What's x? "))
+#y = float(input("What's y? "))
+
+#z = round(x / y, 2)
+
+#print(f"{z:,}")
+
+def main():
+    x = int(input("What's x? "))
+    print("x sqaured is", square(x))
+
+def square(n):
+    return n ** 2
+
+main()    
+
