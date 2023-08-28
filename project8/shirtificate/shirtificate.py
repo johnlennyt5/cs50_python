21ad7be66be0a827a913400aed9d5d07405bfa04
johnlennyt5 <johnlennyt@gmail.com>
2023-08-27 23:13:17 -0400

    work on shirtifcate

--- project8/shirtificate/shirtificate.py
+++ project8/shirtificate/shirtificate.py
@@ -9,7 +9,7 @@ class PDF():
         self._pdf.image("shirtificate.png", w=self._pdf.epw)
         self._pdf.set_font_size(30)
         self._pdf.set_text_color(255, 255, 255)
-        self._pdf.text(x= 47.5, y = 140, txt=f"{name} took CS50")
+        self._pdf.text(x= 47.5, y = 140, txt=f"{name} took CS50!")
 
 
 
